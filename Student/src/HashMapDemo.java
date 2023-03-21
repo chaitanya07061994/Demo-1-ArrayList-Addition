b5115554b0ec174666d0b5031f7b8135d238ef0e
unknown <chaitanyakapse99@gmail.com>
2023-03-21 14:44:07 +0530

    Added HashMap File

---
+++ Student/src/HashMapDemo.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+
+
+public static void main(String[]args) {
+	
+	
+	Map <String,Integer> map = new HashMap<>();
+	
+	map.put("Chaitanya", 99);
+	map.put("Rushikesh", 98);
+	map.put("Amol", 97);
+	map.put("Santosh", 96);
+	map.put("Manoj", 95);
+	
+	System.out.println(map);
+	
+}
+
+
+
+
+
+
+
+
+
+
+ }
